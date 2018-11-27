375a9d30890c5ae55fb86be3fdf8a87c102725de
keremg <keremg0@gmail.com>
2018-11-27 18:31:11 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,15 @@ import { AuthGuardService } from './services/auth-guard.service';
 const routes: Routes = [
   { path: '', redirectTo: 'SignIn', pathMatch: 'full' },
   { path: 'home', loadChildren: './components/home/home.module#HomePageModule' ,canActivate: [AuthGuardService]},
-  { path: 'ChooseTrip', loadChildren: './components/ChooseTrip/ChooseTrip.module#ChooseTripPageModule'  ,canActivate: [AuthGuardService]},
+  { path: 'ChooseTrip', loadChildren: './components/ChooseTrip/ChooseTrip.module#ChooseTripPageModule'},
   { path: 'SignIn', loadChildren: './components/SignIn/SignIn.module#SignInModule' },
-  { path: 'EditEvent', loadChildren: './components/edit-event/edit-event.module#EditEventPageModule'  ,canActivate: [AuthGuardService]},
+  { path: 'EditEvent', loadChildren: './components/edit-event/edit-event.module#EditEventPageModule'},
   { path: 'ResetPassword', loadChildren: './components/reset-password/reset-password.module#ResetPasswordPageModule' },
   { path: 'Signup', loadChildren: './components/signup/signup.module#SignupPageModule' },
-  { path: 'CreateTrip', loadChildren: './components/create-trip/create-trip.module#CreateTripPageModule'  ,canActivate: [AuthGuardService]},
-  { path: 'ViewProfile', loadChildren: './components/view-profile/view-profile.module#ViewProfilePageModule'  ,canActivate: [AuthGuardService]},
-  { path: 'EditProfile', loadChildren: './components/edit-profile/edit-profile.module#EditProfilePageModule'  ,canActivate: [AuthGuardService]},
-  { path: 'Admin', loadChildren: './components/admin/admin.module#AdminPageModule'  ,canActivate: [AuthGuardService]},
+  { path: 'CreateTrip', loadChildren: './components/create-trip/create-trip.module#CreateTripPageModule'},
+  { path: 'ViewProfile', loadChildren: './components/view-profile/view-profile.module#ViewProfilePageModule'},
+  { path: 'EditProfile', loadChildren: './components/edit-profile/edit-profile.module#EditProfilePageModule'  },
+  { path: 'Admin', loadChildren: './components/admin/admin.module#AdminPageModule' },
 
 ];
 
