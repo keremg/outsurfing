788ad1b196942c77aacfc9b56848aeff76e73cfc
keremg <keremg0@gmail.com>
2018-12-01 17:47:35 +0200

    add edit profile page (#14)
    * work in progress
    
    * temp

--- src/app/components/edit-profile/edit-profile.module.ts
+++ src/app/components/edit-profile/edit-profile.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
+
 
 import { EditProfilePage } from './edit-profile.page';
 
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
