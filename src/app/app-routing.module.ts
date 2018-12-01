0fddb8da8041187fa79a8660586f8bdc97c48ccf
gk <keremg0@gmail.com>
2018-12-01 16:43:04 +0200

    new presentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'SignIn', pathMatch: 'full' },
-  { path: 'home', loadChildren: './components/home/home.module#HomePageModule' ,canActivate: [AuthGuardService]},
+  { path: 'home', loadChildren: './components/home/home.module#HomePageModule'},
   { path: 'ChooseRoute', loadChildren: './components/ChooseRoute/ChooseRoute.module#ChooseRoutePageModule'},
   { path: 'SignIn', loadChildren: './components/SignIn/SignIn.module#SignInModule' },
   { path: 'EditEvent', loadChildren: './components/edit-event/edit-event.module#EditEventPageModule'},
