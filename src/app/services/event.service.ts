60478242086d7599bb9edd47f74869ce4c743e3e
gk <keremg0@gmail.com>
2018-12-22 01:25:55 +0200

    event use event

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -3,27 +3,27 @@ import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} 
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import { Action } from 'rxjs/internal/scheduler/Action';
-import {Route} from '../models/Route';
+import {Event} from '../models/Event';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-    tripsCollection: AngularFirestoreCollection<Route>;
-    tripDocRef: AngularFirestoreDocument<Route>;
-    trips: Observable<Route[]>;
-    trip: Observable<Route>;
+    tripsCollection: AngularFirestoreCollection<Event>;
+    tripDocRef: AngularFirestoreDocument<Event>;
+    trips: Observable<Event[]>;
+    trip: Observable<Event>;
 
     constructor(private afs: AngularFirestore) {
         this.tripsCollection = this.afs.collection('trips', ref => ref.orderBy('region', 'asc'));
     }
 
-    getTrips(): Observable<Route[]> {
+    getTrips(): Observable<Event[]> {
       debugger;
         this.trips = this.tripsCollection.snapshotChanges().pipe(map(changes => {
             return changes.map(action => {
-                const data = action.payload.doc.data() as Route;
+                const data = action.payload.doc.data() as Event;
                 data.id = action.payload.doc.id;
                 return data;
             });
@@ -31,18 +31,18 @@ export class EventService {
         return this.trips;
     }
 
-    addTrip(trip: Route) {
+    addTrip(trip: Event) {
         this.tripsCollection.add(trip);
     }
 
 
-    gettrip(id: string): Observable<Route> {
-        this.tripDocRef = this.afs.doc<Route>(`trips/${id}`);
+    gettrip(id: string): Observable<Event> {
+        this.tripDocRef = this.afs.doc<Event>(`trips/${id}`);
         this.trip = this.tripDocRef.snapshotChanges().pipe(map(action => {
             if (action.payload.exists === false) {
                 return null;
             } else {
-                const data = action.payload.data() as Route;
+                const data = action.payload.data() as Event;
                 data.id = action.payload.id;
                 return data;
             }
