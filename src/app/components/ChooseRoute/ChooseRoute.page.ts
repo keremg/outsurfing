38a5e96d8e6fd0ece65fee55a76965c0b0638cb0
Adi Caspi <adicaspi88@gmail.com>
2018-12-29 18:27:46 +0200

    updated choose route

--- src/app/components/ChooseRoute/ChooseRoute.page.ts
+++ src/app/components/ChooseRoute/ChooseRoute.page.ts
@@ -1,51 +1,67 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {SurfRoute} from '../../models/surfRoute';
-import {RouteService} from '../../services/route.service';
-import {NavController} from '@ionic/angular';
-import {PaginationService} from '../../services/pagination.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { SurfRoute } from '../../models/surfRoute';
+import { RouteService } from '../../services/route.service';
+import { NavController } from '@ionic/angular';
+import { PaginationService } from '../../services/pagination.service';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask
+} from '@angular/fire/storage';
 
 @Component({
-    selector: 'app-ChooseRoute',
-    templateUrl: 'ChooseRoute.page.html',
-    styleUrls: ['ChooseRoute.page.scss'],
-    providers:[PaginationService],
+  selector: 'app-ChooseRoute',
+  templateUrl: 'ChooseRoute.page.html',
+  styleUrls: ['ChooseRoute.page.scss'],
+  providers: [PaginationService]
 })
 export class ChooseRoute implements OnInit {
+  constructor(
+    public routeService: RouteService,
+    public navCtrl: NavController,
+    public page: PaginationService,
+    private storage: AngularFireStorage
+  ) {}
 
-    constructor(public routeService: RouteService,
-                public navCtrl: NavController,
-                public page: PaginationService) {
+  ngOnInit() {
+    //this.routesList = this.routeService.getRoutes();
+    this.page.init('routes', 'name', { reverse: true, prepend: false });
+  }
 
+  goToRoute(id) {
+    this.navCtrl.navigateForward('SingleRoute/' + id);
+  }
+
+  createNew() {
+    this.navCtrl.navigateForward('SingleRoute');
+  }
+
+  eventIt(routeId) {
+    this.navCtrl.navigateForward('EventDetail/0/' + routeId);
+  }
+
+  loadData(event) {
+    this.page.more();
+
+    console.log('Done');
+    event.target.complete();
+
+    // App logic to determine if all data is loaded
+    // and disable the infinite scroll
+    //if (this.page.done) {
+    //event.target.disabled = true;
+    //}
+  }
+
+  async routeImageUrl(surfRoute: SurfRoute) {
+    debugger;
+    if (surfRoute.imagesUrls && surfRoute.imagesUrls.length > 0) {
+      const ref = this.storage.ref(
+        'routes/' + surfRoute.id + '/' + surfRoute.imagesUrls[0]
+      );
+      return ref.getDownloadURL();
+    } else {
+      return Promise.resolve(123);
     }
-
-    ngOnInit() {
-        //this.routesList = this.routeService.getRoutes();
-        this.page.init('routes', 'name', {reverse: true, prepend: false});
-    }
-
-    goToRoute(id) {
-        this.navCtrl.navigateForward('SingleRoute/' + id);
-    }
-
-    createNew() {
-        this.navCtrl.navigateForward('SingleRoute');
-    }
-
-    eventIt(routeId) {
-        this.navCtrl.navigateForward('EventDetail/0/' + routeId);
-    }
-
-    loadData(event) {
-        this.page.more();
-
-        console.log('Done');
-        event.target.complete();
-
-        // App logic to determine if all data is loaded
-        // and disable the infinite scroll
-        //if (this.page.done) {
-        //event.target.disabled = true;
-        //}
-    }
+  }
 }
--- src/app/models/surfRoute.ts
+++ src/app/models/surfRoute.ts
@@ -1,37 +1,38 @@
-import {SurfUser} from './surfUser';
+import { SurfUser } from './surfUser';
 
 export class SurfRoute {
-    id: string;
-    name: string;
-    country: string;
-    state: string;
-    routeStartLocation: string;
-    routeEndLocation: string;
-    routeStartGeolocation: string;
-    routeEndGeolocation: string;
+  id: string;
+  name: string;
+  country: string;
+  state: string;
+  routeStartLocation: string;
+  routeEndLocation: string;
+  routeStartGeolocation: string;
+  routeEndGeolocation: string;
 
-    imagesUrls: string[];
-    mapUrl: string;
-    lengthKM: number;
-    shortDescription: string;
-    longDescription: string; // details
-    //routeRanking: {ranking: number, review: string}[];
-    routeDifficulty: string; // Level:  0 - very easy, 1-easy, 2-moderate, 3-challenging, 4-extreme, 5-very extreme
-    routeDuration: number; // will represent number of days, so half day should be 0.5 , one hour should be 0.04
-    routeProperties: string[]; // -	e.g. water, swimming, mountains, bicycles, forest, desert, oasis, historical, archeology, ropes
+  imagesUrls: string[];
+  mapUrl: string;
+  lengthKM: number;
+  shortDescription: string;
+  longDescription: string; // details
+  //routeRanking: {ranking: number, review: string}[];
+  routeRanking: number;
+  routeNumOfRankers: number;
+  routeDifficulty: string; // Level:  0 - very easy, 1-easy, 2-moderate, 3-challenging, 4-extreme, 5-very extreme
+  routeDuration: number; // will represent number of days, so half day should be 0.5 , one hour should be 0.04
+  routeProperties: string[]; // -	e.g. water, swimming, mountains, bicycles, forest, desert, oasis, historical, archeology, ropes
 
-    routeCreatorId: string;
-    routeCreator?: SurfUser;
-    isGuidingOffered: boolean;
-    offeredPrice: number; // default 0, more if isGuidingOffered
-    guideContactDetails: string; // ree text allowing the guide to describe email and/or phone for contacting him/her about guidance
-    entranceFee: number;
-    requiredEquipment: string;
-    numEventsCreatedFromRoute: number;
-    recommendedMonths: string[];
+  routeCreatorId: string;
+  routeCreator?: SurfUser;
+  isGuidingOffered: boolean;
+  offeredPrice: number; // default 0, more if isGuidingOffered
+  minGroupSizeForGuide: number; //TODO - add to html
+  guideContactDetails: string; // ree text allowing the guide to describe email and/or phone for contacting him/her about guidance
+  entranceFee: number;
+  requiredEquipment: string;
+  numEventsCreatedFromRoute: number;
+  recommendedMonths: string[];
 
-    iconName:string; //TODO terrain
-    seasons:string;//TODO
-
-
+  iconName: string; //TODO terrain
+  seasons: string; //TODO
 }
