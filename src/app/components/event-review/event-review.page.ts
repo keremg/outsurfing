ddb24c388943cfcd62488a82cfab0a9e13e56323
nir <nirsivan204@gmail.com>
2019-01-08 00:44:32 +0200

    event review

--- src/app/components/event-review/event-review.page.ts
+++ src/app/components/event-review/event-review.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AudienceTypeEnum} from '../../AudienceType.enum';
 
 @Component({
   selector: 'app-event-review',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-review.page.scss'],
 })
 export class EventReviewPage implements OnInit {
+    audiences : AudienceTypeEnum[];
+    audiences_strings : string[];
+    audience_rating : {type:string,rating:Number} [];
+    event_reviews: number[];
+    isGuided: boolean;
+    guideName: string;
+    guide_reviews: number[];
+    participants_rating: {name:string, rating:number} [];
+    participants : string[];
+  constructor() {
+      this.event_reviews = [0,0];
+      this.audiences = [AudienceTypeEnum.Elderlies,AudienceTypeEnum.Couples];
+      this.audiences_strings = ['Elderlies','Couples'];
+      this.audience_rating = [];
+      this.audience_rating.push({type: 'Elderlies', rating: 0});
+      this.audience_rating.push({type: 'Couples', rating: 0});
+      this.isGuided =true;
+      this.guideName = "madrichush";
+      this.guide_reviews = [0,0];
+      this.participants = ['dani','avi'];
+      this.participants_rating = [];
+      this.participants_rating.push({name:'dani', rating: 0});
+      this.participants_rating.push({name:'avi', rating: 0});
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
 
+    submit(){
+        console.log( this.participants_rating['avi'])
+    }
 }
